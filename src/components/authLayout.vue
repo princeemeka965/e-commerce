309214d867e271c890f70edc2a57247b869bdd59
princeemeka965 <anyanwue4@gmail.com>
2024-08-17 15:44:52 +0100

    designed the login view

---
+++ src/components/authLayout.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div className="lg:flex md:flex hidden lg:w-1/2 md:w-1/2 w-full flex-grow h-screen p-5 bg-market">
+    <div className="flex w-full h-full flex-col justify-center">
+      <div className="flex flex-row justify-end">
+        <div className="lg:w-3/4 md:w-3/4 w-full flex flex-col my-10 div-right"></div>
+      </div>
+    </div>
+  </div>
+</template>
