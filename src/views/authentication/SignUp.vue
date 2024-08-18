f48e5acb6b2b2a07c6281133d9c783aa71d0c508
princeemeka965 <anyanwue4@gmail.com>
2024-08-18 18:44:36 +0100

    applied validation to sign upform

--- src/views/authentication/SignUp.vue
+++ src/views/authentication/SignUp.vue
@@ -1,5 +1,30 @@
 <script setup>
+import { reactive, toRefs } from 'vue'
+import useVuelidate from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
 import AuthLayout from '@/components/authLayout.vue'
+
+const state = reactive({
+  name: '',
+  email: '',
+  password: ''
+})
+const rules = {
+  name: { required },
+  email: { required, email },
+  password: { required }
+}
+
+const v$ = useVuelidate(rules, state)
+
+async function submitForm() {
+  const isFormCorrect = await v$.value.$validate()
+  if (!isFormCorrect) {
+    return
+  }
+  // Perform the actual form submission (e.g., API call)
+  alert('Form submitted successfully!')
+}
 </script>
 
 <template>
@@ -23,21 +48,30 @@ import AuthLayout from '@/components/authLayout.vue'
                 >
                   Create an account
                 </h4>
-                <form class="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96">
+                <form class="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96" @submit.prevent="submitForm">
                   <div class="flex flex-col gap-6 mb-1">
                     <h6
                       class="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900"
                     >
                       Your Name
                     </h6>
-                    <div class="relative h-11 w-full min-w-[200px]">
+                    <div class="relative min-h-11 w-full min-w-[200px]">
                       <input
                         placeholder="Enter your name"
-                        class="peer h-full w-full rounded-md border border-blue-gray-200 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                        v-model="state.name"
+                        :class="
+                          v$.name.$invalid && v$.name.$dirty ? 'border-red-500 border-2' : 'border'
+                        "
+                        class="peer h-full w-full rounded-md border-blue-gray-200 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                       />
                       <label
                         class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
                       ></label>
+                      <div v-if="v$.name.$invalid && v$.name.$dirty">
+                        <span v-for="(error, index) in v$.name.$errors" :key="index" class="capitalize text-xs text-red-500">
+                         {{ error.$property }} {{ error.$message }}
+                        </span>
+                      </div>
                     </div>
 
                     <h6
@@ -45,34 +79,56 @@ import AuthLayout from '@/components/authLayout.vue'
                     >
                       Your Email
                     </h6>
-                    <div class="relative h-11 w-full min-w-[200px]">
+                    <div class="relative min-h-11 w-full min-w-[200px]">
                       <input
                         placeholder="name@mail.com"
+                        v-model="state.email"
+                        :class="
+                          v$.email.$invalid && v$.email.$dirty ? 'border-red-500 border-2' : 'border'
+                        "
                         class="peer h-full w-full rounded-md border border-blue-gray-200 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                       />
                       <label
                         class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
                       ></label>
+                      <div v-if="v$.email.$invalid && v$.email.$dirty">
+                        <span v-for="(error, index) in v$.email.$errors" :key="index" class="capitalize text-xs text-red-500">
+                         {{ error.$property }} {{ error.$message }}
+                        </span>
+                      </div>
                     </div>
+
+
                     <h6
                       class="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900"
                     >
                       Password
                     </h6>
-                    <div class="relative h-11 w-full min-w-[200px]">
+                    <div class="relative min-h-11 w-full min-w-[200px]">
                       <input
                         type="password"
+                        v-model="state.password"
                         placeholder="********"
+                        :class="
+                          v$.password.$invalid && v$.password.$dirty ? 'border-red-500 border-2' : 'border'
+                        "
                         class="peer h-full w-full rounded-md border border-blue-gray-200 !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                       />
                       <label
                         class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
                       ></label>
+                      <div v-if="v$.password.$invalid && v$.password.$dirty">
+                        <span v-for="(error, index) in v$.password.$errors" :key="index" class="capitalize text-xs text-red-500">
+                         {{ error.$property }} {{ error.$message }}
+                        </span>
+                      </div>
                     </div>
                   </div>
+
+
                   <button
                     class="mt-6 block w-full select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                    type="button"
+                    type="submit"
                   >
                     Sign Up
                   </button>
